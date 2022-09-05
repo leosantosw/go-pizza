3252b0284c694e5658af066896109fa961f55a3f
Leonardo <leonardosantosx0077@gmail.com>
2022-09-04 22:17:40 -0300

    add firebase integration and styled-components

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import theme from './src/theme';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components/native';
 
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
+import { SignIn } from '@screens/SignIn';
+import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+export function App() {
 
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
     DMSerifDisplay_400Regular,
   })
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  if (fontsLoaded) {
+    SplashScreen.hideAsync();
   }
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+      <SignIn />
     </ThemeProvider>
   );
 }
